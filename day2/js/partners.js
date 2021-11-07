98f55621e163e65d514eeb4907af0c836d1bdc52
Amind <amind@bk.ru>
2021-11-07 07:48:01 +0400

    Edit Day2

--- day2/js/partners.js
+++ day2/js/partners.js
@@ -1,13 +1,13 @@
 function renderItems(data) {
     data.forEach((element) => {
         console.log(
-            "Name: " + element.name +
-            ", time_of_delivery: " + element.time_of_delivery +
-            ", stars: " + element.stars +
-            ", price: " + element.price +
-            ", kitchen: " + element.kitchen +
-            ", image: " + element.image +
-            ", product: " + element.products
+            `Name: ${element.name}
+            time_of_delivery: ${element.time_of_delivery}
+            stars: ${element.stars}
+            price: ${element.price}
+            kitchen: ${element.kitchen}
+            image: ${element.image}
+            product: ${element.products}`
         )
     });
 }
